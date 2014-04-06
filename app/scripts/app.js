d6df67f1f80a91b183f05be93e79b24addbb60fd
Brad Oyler <bradoyler@gmail.com>
2014-04-06 01:18:59 -0400

    add flags

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,17 +7,19 @@ angular
     'ngSanitize',
     'ngRoute'
   ])
-  .config(function($routeProvider, $anchorScrollProvider) {
+  .config(function($routeProvider, $anchorScrollProvider, $locationProvider) {
 
    // $anchorScrollProvider.disableAutoScrolling();
 
+   // $locationProvider.html5Mode(true).hashPrefix('!');
+
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: '/views/main.html',
         controller: 'MainCtrl'
       })
       .when('/article/:externalId', {
-        templateUrl: 'views/article.html',
+        templateUrl: '/views/article.html',
         controller: 'ArticleCtrl'
       })
       .otherwise({
